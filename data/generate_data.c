59c8e82c97a458f4c69064c4e96fe907f032ba8a
muyuliyan <9110124031@email.ncu.edu.cn>
2024-10-16 09:16:38 +0800

    clected some data

---
+++ data/generate_data.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void generate_random_data(const char* filename, int num_elements, int is_float) {
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        perror("Unable to open file");
+        return;
+    }
+
+    srand((unsigned int)time(NULL));
+
+    for (int i = 0; i < num_elements; ++i) {
+        if (is_float) {
+            double random_float = (double)rand() / RAND_MAX * 10000;
+            fprintf(file, "%f\n", random_float);
+        } else {
+            int random_int = rand() % 10000;
+            fprintf(file, "%d\n", random_int);
+        }
+    }
+
+    fclose(file);
+}
+
+int main() {
+    // Generate different scales of data
+    generate_random_data("small_data.txt", 100, 0);
+    generate_random_data("medium_data.txt", 1000, 0);
+    generate_random_data("large_data.txt", 100000, 0);
+    generate_random_data("large_data_float.txt", 100000, 1);
+
+    return 0;
+}
